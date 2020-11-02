7058687d9996b0b1dabb771c3df29fca43b05d77
tobozo <tobozo@users.noreply.github.com>
2020-11-02 15:24:36 +0100

    Procrastinating next data storage

--- BLECache.h
+++ BLECache.h
@@ -29,6 +29,36 @@
 
 */
 
+
+/*\
+ *
+
+  // TODO: store cache items as vectors in PSRam
+
+  template <class T>
+  struct PSallocator {
+    typedef T value_type;
+    PSallocator() = default;
+    template <class U> constexpr PSallocator(const PSallocator<U>&) noexcept {}
+    [[nodiscard]] T* allocate(std::size_t n) {
+      if(n > std::size_t(-1) / sizeof(T)) throw std::bad_alloc();
+      if(auto p = static_cast<T*>(ps_malloc(n*sizeof(T)))) return p;
+      throw std::bad_alloc();
+    }
+    void deallocate(T* p, std::size_t) noexcept { std::free(p); }
+  };
+  template <class T, class U>
+  bool operator==(const PSallocator<T>&, const PSallocator<U>&) { return true; }
+  template <class T, class U>
+  bool operator!=(const PSallocator<T>&, const PSallocator<U>&) { return false; }
+
+  std::vector<int, PSallocator<int> > v;
+
+ *
+\*/
+
+
+
 struct BLEGATTService {
   const char* name;
   const char* type;
