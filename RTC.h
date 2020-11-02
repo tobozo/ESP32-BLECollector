b67db5bc3662315a27c01aa4bb35904685894674
tobozo <tobozo@users.noreply.github.com>
2020-11-02 16:29:59 +0100

    Wrapped BM8563 RTC driver for M5Core2

--- RTC.h
+++ RTC.h
@@ -37,8 +37,9 @@
 
 #if defined( ARDUINO_M5STACK_Core2 ) // M5Core2 uses BM8563
 
-  #define BM8563_ADDR 0x51 // M5Core2 RTC I2C address
+  //#define BM8563_ADDR 0x51 // M5Core2 RTC I2C address
   #define BLE_RTC BLE_RTC_BM8563 // alias for BLECollector
+  #define M5_RTC M5.Rtc // syntax sugar
 
   class BLE_RTC_BM8563 {
     public:
@@ -59,129 +60,104 @@
 
   // but but but why u no put code in cpp file ??? :-))
 
-  static uint8_t Bcd2ToByte(uint8_t Value) {
-    uint8_t tmp = 0;
-    tmp = ((uint8_t)(Value & (uint8_t)0xF0) >> (uint8_t)0x4) * 10;
-    return (tmp + (Value & (uint8_t)0x0F));
+  void BLE_RTC_BM8563::GetTime(tmElements_t &RTC_TimeStruct)
+  {
+    RTC_TimeTypeDef tmpTime;
+    M5_RTC.GetTime( &tmpTime );
+    RTC_TimeStruct.Second  = tmpTime.Seconds;
+    RTC_TimeStruct.Minute  = tmpTime.Minutes;
+    RTC_TimeStruct.Hour    = tmpTime.Hours;
   }
 
-  static uint8_t ByteToBcd2(uint8_t Value) {
-    uint8_t bcdhigh = 0;
-    while (Value >= 10) {
-      bcdhigh++;
-      Value -= 10;
-    }
-    return  ((uint8_t)(bcdhigh << 4) | Value);
+
+  void BLE_RTC_BM8563::GetDate(tmElements_t &RTC_DateStruct)
+  {
+    RTC_DateTypeDef tmpDate;
+    M5_RTC.GetDate( &tmpDate );
+    RTC_DateStruct.Day   = tmpDate.Date;
+    RTC_DateStruct.Wday  = tmpDate.WeekDay;
+    RTC_DateStruct.Month = tmpDate.Month;
+    RTC_DateStruct.Year  = tmpDate.Year - 1970;
   }
 
-  void BLE_RTC_BM8563::GetTime(tmElements_t &RTC_TimeStruct) {
-    uint8_t buf[3] = {0};
-    Wire1.beginTransmission(0x51);
-    Wire1.write(0x02);
-    Wire1.endTransmission();
-    Wire1.requestFrom(0x51,3);
-    while(Wire1.available()){
-      buf[0] = Wire1.read();
-      buf[1] = Wire1.read();
-      buf[2] = Wire1.read();
-    }
-    RTC_TimeStruct.Second  = Bcd2ToByte(buf[0]&0x7f);
-    RTC_TimeStruct.Minute  = Bcd2ToByte(buf[1]&0x7f);
-    RTC_TimeStruct.Hour    = Bcd2ToByte(buf[2]&0x3f);
-  }
 
-  void BLE_RTC_BM8563::GetDate(tmElements_t &RTC_DateStruct) {
-    uint8_t buf[4] = {0};
-    Wire1.beginTransmission(0x51);
-    Wire1.write(0x05);
-    Wire1.endTransmission();
-    Wire1.requestFrom(0x51,4);
-    while(Wire1.available()){
-        buf[0] = Wire1.read();
-        buf[1] = Wire1.read();
-        buf[2] = Wire1.read();
-        buf[3] = Wire1.read();
-    }
-    RTC_DateStruct.Day   = Bcd2ToByte(buf[0]&0x3f);
-    RTC_DateStruct.Wday  = Bcd2ToByte(buf[1]&0x07);
-    RTC_DateStruct.Month = Bcd2ToByte(buf[2]&0x1f);
-
-    RTC_DateStruct.Year = Bcd2ToByte(buf[3]&0xff);
-
-  }
-
-  void BLE_RTC_BM8563::SetTime(const tmElements_t *RTC_TimeStruct) {
+  void BLE_RTC_BM8563::SetTime(const tmElements_t *RTC_TimeStruct)
+  {
     if(RTC_TimeStruct == NULL)
       return;
-    Wire1.beginTransmission(0x51);
-    Wire1.write(0x02);
-    Wire1.write(ByteToBcd2(RTC_TimeStruct->Second));
-    Wire1.write(ByteToBcd2(RTC_TimeStruct->Minute));
-    Wire1.write(ByteToBcd2(RTC_TimeStruct->Hour));
-    Wire1.endTransmission();
+
+    RTC_TimeTypeDef tmpTime;
+    tmpTime.Hours   = RTC_TimeStruct->Hour;
+    tmpTime.Minutes = RTC_TimeStruct->Minute;
+    tmpTime.Seconds = RTC_TimeStruct->Second;
+    M5_RTC.SetTime( &tmpTime );
   }
 
-  void BLE_RTC_BM8563::SetDate(const tmElements_t *RTC_DateStruct) {
 
+  void BLE_RTC_BM8563::SetDate(const tmElements_t *RTC_DateStruct)
+  {
     if(RTC_DateStruct == NULL)
       return;
-    Wire1.beginTransmission(0x51);
-    Wire1.write(0x05);
-    Wire1.write(ByteToBcd2(RTC_DateStruct->Day));
-    Wire1.write(ByteToBcd2(RTC_DateStruct->Wday));
 
-    if(RTC_DateStruct->Year < 2000){
-      Wire1.write(ByteToBcd2(RTC_DateStruct->Month) | 0x80);
-      Wire1.write(ByteToBcd2((uint8_t)(RTC_DateStruct->Year % 100)));
-    } else {
-      Wire1.write(ByteToBcd2(RTC_DateStruct->Month) | 0x00);
-      Wire1.write(ByteToBcd2((uint8_t)(RTC_DateStruct->Year %100)));
-    }
-    Wire1.endTransmission();
+    RTC_DateTypeDef tmpDate;
+    tmpDate.Date    = RTC_DateStruct->Day;
+    tmpDate.WeekDay = RTC_DateStruct->Wday;
+    tmpDate.Month   = RTC_DateStruct->Month;
+    tmpDate.Year    = RTC_DateStruct->Year + 1970;
+    log_w("Year value: %d", RTC_DateStruct->Year );
+    M5_RTC.SetDate( &tmpDate );
   }
 
-  bool BLE_RTC_BM8563::begin(uint8_t sdaPin, uint8_t sclPin) {
-    Wire1.begin(sdaPin, sclPin);
+
+  bool BLE_RTC_BM8563::begin(uint8_t sdaPin, uint8_t sclPin)
+  {
+    M5_RTC.begin();
     return true;
   }
 
-  void BLE_RTC_BM8563::adjust(const DateTime& dt) {
+
+  void BLE_RTC_BM8563::adjust(const DateTime& dt)
+  {
     dumpTime("RTC Will adjust from DateTime ", dt.get_tm());
     adjust( dt.get_tm() );
   }
 
-  void BLE_RTC_BM8563::adjust(const time_t& dt) {
+
+  void BLE_RTC_BM8563::adjust(const time_t& dt)
+  {
     tmElements_t dateTimeNow;
     breakTime(dt, dateTimeNow);
     dumpTime("RTC Will adjust from time_t ", dateTimeNow );
     adjust( dateTimeNow );
   }
 
-  uint32_t BLE_RTC_BM8563::unixtime() {
+
+  uint32_t BLE_RTC_BM8563::unixtime()
+  {
     return DateTime::tm2unixtime( now() );
   }
 
-  uint8_t BLE_RTC_BM8563::isrunning(void) {
-    Wire1.beginTransmission(BM8563_ADDR);
-    Wire1.write(0x02);
-    Wire1.endTransmission();
-    Wire1.requestFrom(BM8563_ADDR, 1);
-    uint8_t ss = Wire1.read();
-    return !(ss>>7);
+
+  uint8_t BLE_RTC_BM8563::isrunning(void)
+  {
+    return M5_RTC.isrunning();
   }
 
-  void BLE_RTC_BM8563::adjust(const tmElements_t& dt) {
+
+  void BLE_RTC_BM8563::adjust(const tmElements_t& dt)
+  {
     SetTime( &dt );
     SetDate( &dt );
   }
 
-  tmElements_t BLE_RTC_BM8563::now() {
+
+  tmElements_t BLE_RTC_BM8563::now()
+  {
     tmElements_t thisTime;
     GetTime( thisTime );
     GetDate( thisTime );
     return thisTime;
   }
-
 
 
 
@@ -209,7 +185,7 @@
 
 
   bool BLE_RTC_DS1307::begin(uint8_t sdaPin, uint8_t sclPin) {
-    Wire.begin(sdaPin, sclPin);
+    Wire1.begin(sdaPin, sclPin);
     return true;
   }
   void BLE_RTC_DS1307::adjust(const DateTime& dt) {
@@ -226,39 +202,39 @@
     return DateTime::tm2unixtime( now() );
   }
   uint8_t BLE_RTC_DS1307::isrunning(void) {
-    Wire.beginTransmission(DS1307_ADDR);
-    Wire.write(ZEROINT);
-    Wire.endTransmission();
-    Wire.requestFrom(DS1307_ADDR, 1);
-    uint8_t ss = Wire.read();
+    Wire1.beginTransmission(DS1307_ADDR);
+    Wire1.write(ZEROINT);
+    Wire1.endTransmission();
+    Wire1.requestFrom(DS1307_ADDR, 1);
+    uint8_t ss = Wire1.read();
     return !(ss>>7);
   }
   void BLE_RTC_DS1307::adjust(const tmElements_t& dt) {
     //dumpTime("RTC Will adjust from tmElements_t ", dt );
-    Wire.beginTransmission(DS1307_ADDR);
-    Wire.write(ZEROINT);
-    Wire.write(BLE_RTC_bin2bcd(dt.Second));
-    Wire.write(BLE_RTC_bin2bcd(dt.Minute));
-    Wire.write(BLE_RTC_bin2bcd(dt.Hour));
-    Wire.write(BLE_RTC_bin2bcd(0));
-    Wire.write(BLE_RTC_bin2bcd(dt.Day));
-    Wire.write(BLE_RTC_bin2bcd(dt.Month));
-    Wire.write(BLE_RTC_bin2bcd(dt.Year)); // 2000 to 1970 offset
-    Wire.write(ZEROINT);
-    Wire.endTransmission();
+    Wire1.beginTransmission(DS1307_ADDR);
+    Wire1.write(ZEROINT);
+    Wire1.write(BLE_RTC_bin2bcd(dt.Second));
+    Wire1.write(BLE_RTC_bin2bcd(dt.Minute));
+    Wire1.write(BLE_RTC_bin2bcd(dt.Hour));
+    Wire1.write(BLE_RTC_bin2bcd(0));
+    Wire1.write(BLE_RTC_bin2bcd(dt.Day));
+    Wire1.write(BLE_RTC_bin2bcd(dt.Month));
+    Wire1.write(BLE_RTC_bin2bcd(dt.Year)); // 2000 to 1970 offset
+    Wire1.write(ZEROINT);
+    Wire1.endTransmission();
   }
   tmElements_t BLE_RTC_DS1307::now() {
-    Wire.beginTransmission(DS1307_ADDR);
-    Wire.write(ZEROINT);
-    Wire.endTransmission();
-    Wire.requestFrom(DS1307_ADDR, 7);
-    uint8_t ss = BLE_RTC_bcd2bin(Wire.read() & 0x7F);
-    uint8_t mm = BLE_RTC_bcd2bin(Wire.read());
-    uint8_t hh = BLE_RTC_bcd2bin(Wire.read());
-    Wire.read();
-    uint8_t d = BLE_RTC_bcd2bin(Wire.read());
-    uint8_t m = BLE_RTC_bcd2bin(Wire.read());
-    uint8_t y = BLE_RTC_bcd2bin(Wire.read()); // 2000 to 1970 offset
+    Wire1.beginTransmission(DS1307_ADDR);
+    Wire1.write(ZEROINT);
+    Wire1.endTransmission();
+    Wire1.requestFrom(DS1307_ADDR, 7);
+    uint8_t ss = BLE_RTC_bcd2bin(Wire1.read() & 0x7F);
+    uint8_t mm = BLE_RTC_bcd2bin(Wire1.read());
+    uint8_t hh = BLE_RTC_bcd2bin(Wire1.read());
+    Wire1.read();
+    uint8_t d = BLE_RTC_bcd2bin(Wire1.read());
+    uint8_t m = BLE_RTC_bcd2bin(Wire1.read());
+    uint8_t y = BLE_RTC_bcd2bin(Wire1.read()); // 2000 to 1970 offset
     return tmElements_t {ss, mm, hh, 0, d, m, y};
   }
 
